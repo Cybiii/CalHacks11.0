4a1f69be63db5aec48c56782ab30a49750d7d505
Rafa <rafumojica@gmail.com>
2024-10-19 04:19:57 -0700

    added firebase auth

---
+++ src/config/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC8PD-lH7Kn0k3BgYp6QI06e0q5Ee4C4Xo",
+  authDomain: "calhacks11-52247.firebaseapp.com",
+  projectId: "calhacks11-52247",
+  storageBucket: "calhacks11-52247.appspot.com",
+  messagingSenderId: "295453296860",
+  appId: "1:295453296860:web:d4b8437eff0d7ca97a4c19",
+  measurementId: "G-DSVS65B6R6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app)
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import postcss from './postcss.config.js'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import postcss from "./postcss.config.js";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': process.env
+    "process.env": process.env,
   },
   css: {
     postcss,
@@ -19,11 +19,14 @@ export default defineConfig({
           return val.replace(/^~/, "");
         },
       },
+      {
+        "@": path.resolve(__dirname, "./src"),
+      },
     ],
   },
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
-    }
-  } 
-})
+    },
+  },
+});
